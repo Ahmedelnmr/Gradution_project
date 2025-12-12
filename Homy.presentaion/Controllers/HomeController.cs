6f9371ca6f918026cfd0708e20c1235aef33deb4
ahmedelnmer <ahmedelnmet011@gmail.com>
2025-12-13 01:38:13 +0200

    create admin dasbourd contol panel

--- Homy.presentaion/Controllers/HomeController.cs
+++ Homy.presentaion/Controllers/HomeController.cs
@@ -1,21 +1,39 @@
 using Homy.presentaion.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Homy.Domin.models;
+using Homy.Domin.Contract_Repo;
+using Microsoft.AspNetCore.Identity;
+using Homy.Application.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace Homy.presentaion.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager;
+        private readonly IProperty_Repo _propertyRepo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, IProperty_Repo propertyRepo)
         {
             _logger = logger;
+            _userManager = userManager;
+            _propertyRepo = propertyRepo;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var usersCount = await _userManager.Users.CountAsync();
+            var adsCount = await _propertyRepo.CountAsync();
+
+            var dashboardDto = new DashboardDto
+            {
+                UsersCount = usersCount,
+                AdsCount = adsCount
+            };
+
+            return View(dashboardDto);
         }
 
         public IActionResult Privacy()
